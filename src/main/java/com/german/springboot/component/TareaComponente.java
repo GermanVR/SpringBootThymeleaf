f0805fafd1c869dc5fa156b8d0c90a051ca47afd
German <german_1241@hotmail.com>
2018-08-13 16:11:34 -0500

    Agregado Spring Scheduled

---
+++ src/main/java/com/german/springboot/component/TareaComponente.java
@@ -0,0 +1,19 @@
+package com.german.springboot.component;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TareaComponente {
+
+	private static final Log log = LogFactory.getLog(TareaComponente.class);
+
+	@Scheduled(fixedDelay = 1000)
+	public void ejecutaTarea() {
+		log.info("La hora es: " + new Date());
+	}
+}
